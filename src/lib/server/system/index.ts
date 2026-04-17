bf2a656085d856dd73874d02044c69d00d8bf2c3
Joel <joeljmanuel@gmail.com>
2026-04-17 17:34:46 -0400

    omx(team): auto-checkpoint worker-1 [1]

--- src/lib/server/system/index.ts
+++ src/lib/server/system/index.ts
@@ -2,5 +2,6 @@ import "server-only";
 
 export { getDisplayState, handleDisplayAction } from "./display";
 export { getWifiStatus, handleWifiAction } from "./wifi";
+export { getAudioStatus, handleAudioAction } from "./audio";
 export { loadSystemConfig } from "./config";
 export { appendAudioTestLog, readRecentAudioTestLogs } from "./audio-test-log";
